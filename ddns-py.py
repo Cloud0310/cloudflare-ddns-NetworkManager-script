4ce6e45cd2f8c77471d4ed25521015ecf113d14b
Cloud0310 <60375730+Cloud0310@users.noreply.github.com>
2025-10-04 01:02:30 +0800

    added: color log

--- ddns-py.py
+++ ddns-py.py
@@ -11,9 +11,39 @@ from time import sleep
 from typing import Any
 from urllib import error, parse, request
 
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    )
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler(stdout)
+
+console_handler.setFormatter(CustomFormatter())
+
 logging.basicConfig(
     level=logging.INFO,
-    handlers=[logging.StreamHandler(stdout)],
+    handlers=[console_handler],
 )
 
 parser = ArgumentParser(
